0f7d3da144f960d5db4b2c7c67157821c7760766
sullivan <SullivanMuse@users.noreply.github.com>
2023-12-07 06:37:11 -0600

    Initial commit

---
+++ getExternal.py
@@ -0,0 +1,56 @@
+import tool.pythonCheck as pc
+import os
+import sys
+import json
+from pathlib import Path
+
+required_module = {
+    'colorama': 'colorama',
+    'gitpython': 'git',
+    'tqdm': 'tqdm'
+}
+
+try:
+    assert(pc.python_version() == 3), "Python version required at least 3.0"
+    print("Python version: ", sys.version)
+except Exception as err:
+    print(err)
+    input("Press the Enter to continue")
+    sys.exit(1)
+
+print('-----------------------------------------------------')
+for module, module_name in required_module.items():
+    print(f"Checking the module {module}")
+    if pc.is_module_exist(module_name) == False:
+        pc.install_module(module)
+    else:
+        print(
+            f"Module {module} has been installed!!!")
+    print('-----------------------------------------------------')
+
+import tool.gitUtils as git
+from colorama import Fore
+
+# Read the file contain external repo
+file = Path("./external.json")
+
+try:
+    assert(file.is_file()), Fore.RED+f'The file {file.name} does not exist!'
+except Exception as err:
+    print(err)
+    input('Press Enter to continue')
+    sys.exit(1)
+
+with open(file, 'r') as repo_data:
+    data = json.load(repo_data)
+for lib in data["external_lib"]:
+    repo_name = lib["repo_name"]
+    repo_url = lib["repo_url"]
+    clone_location = lib["clone_location"]
+    clone_branch = lib["clone_branch"]
+    if os.path.isdir(clone_location):
+        print(Fore.MAGENTA +
+              f"The folder {repo_name} has been existed" + Fore.RESET)
+    else:
+        git.clone_repo(repo_name, repo_url, clone_location, clone_branch)
+print('-----------------------------------------------------')
---
+++ src/Main.cpp
@@ -0,0 +1,119 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <unistd.h>
+
+#include <header/constantList.h>
+#include <header/appUtils.h>
+#include <header/shader.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    GLFWwindow *window;
+    
+    char cwd[PATH_MAX];
+    string address;
+    if(getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        address = cwd;
+    }
+    else
+    {
+        perror("getcwd() error");
+        return 1;
+    }
+
+    init();
+
+    if (!createWindow(window, SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL"))
+    {
+        cout << "Failed to create window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    string vertexString = address + "\\src\\shader\\vertex.shader";
+    const char *vertexPath = vertexString.c_str();
+    string fragmentString = address + "\\src\\shader\\fragment.shader";
+    const char *fragmentPath = fragmentString.c_str();
+    // build and compile our shader program
+    // ------------------------------------
+    Shader ourShader(vertexPath, fragmentPath, nullptr); // you can name your shader files however you like
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    // glBindVertexArray(0);
+
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // render the triangle
+        ourShader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
